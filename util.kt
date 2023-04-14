d8c16e3c294693106dc2f3e3d7a223993a0129fa
KruASe <55000336+KruASe76@users.noreply.github.com>
2023-04-14 18:17:26 +0300

    Create util.kt

---
+++ util.kt
@@ -0,0 +1,78 @@
+import java.io.BufferedWriter
+import kotlin.math.sqrt
+import kotlin.math.pow
+
+
+val Int.isEven: Boolean
+    get() = this % 2 == 0
+
+val Int.isPrime: Boolean
+    get() {
+        for (delimiter in 2..sqrt(this.toDouble()).toInt()) {
+            if (this % delimiter == 0) return false
+        }
+
+        return true
+    }
+
+fun Int.pow(n: Int): Int {
+    return this.toDouble().pow(n).toInt()
+}
+
+
+fun c(n: Long, k: Long): Long {
+    return factorial(n) / (factorial(k) * factorial(n - k))
+}
+
+fun factorial(n: Long): Long {
+    var result = 1L
+    for (i in 2..n) {
+        result *= i
+    }
+    return result
+}
+
+
+fun <E> Collection<E>.isSub(collection: Collection<E>): Boolean {
+    for (element in this.toSet()) {
+        if (collection.count { it == element } < this.count { it == element }) {
+            return false
+        }
+    }
+    return true
+}
+
+fun <T> List<List<T>>.tableString(columnSeparator: String = "\t", rowSeparator: String = "\n"): String {
+    return this.joinToString(rowSeparator) { it.joinToString(columnSeparator) }
+}
+
+
+fun BufferedWriter.writeLn(line: String) {
+    write(line)
+    newLine()
+}
+
+
+/**
+ * Returns a list of lists, each built from elements of all lists with the same indexes.
+ * Output has length of the shortest input list.
+ */
+fun <T> zip(vararg lists: List<T>): List<List<T>> {
+    return zip(*lists, transform = { it })
+}
+
+/**
+ * Returns a list of values built from elements of all lists with same indexes using provided [transform].
+ * Output has length of the shortest input list.
+ */
+fun <T, V> zip(vararg lists: List<T>, transform: (List<T>) -> V): List<V> {
+    val minSize = lists.minOfOrNull(List<T>::size) ?: return emptyList()
+    val list = ArrayList<V>(minSize)
+
+    val iterators = lists.map { it.iterator() }
+    repeat(minSize) {
+        list.add(transform(iterators.map { it.next() }))
+    }
+
+    return list
+}
