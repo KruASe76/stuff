9900600e106edb016286d131d4847f8444c608ef
KruASe <55000336+KruASe76@users.noreply.github.com>
2023-12-04 16:05:32 +0300

    Update local_pip.sh

--- local_pip.sh
+++ local_pip.sh
@@ -1,1 +1,1 @@
-curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py --user && echo -e "\nexport PATH=\$PATH:\$HOME/.local/bin" >> ~/.bashrc
+curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py --user && echo -e "\nexport PATH=\$PATH:\$HOME/.local/bin" >> ~/.bashrc && source ~/.bashrc
